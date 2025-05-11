4eb945ea73421eff5661fd899dec89412bfa1a4c
wjz2001 <wjz2001@users.noreply.github.com>
2025-05-11 15:09:18 +0800

    修改版本号及apk名生成逻辑

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
+import org.gradle.api.JavaVersion
+
+// 获取当前年月日时分，转化为版本号
+fun getVersionName(): String {
+    // 获取当前日期与时间
+    val currentDate = LocalDateTime.now()
+    // 格式化为两位年份、两位月份、两位日子、两位小时、两位分钟，使用中国时区
+    val versionDateTimeFormatter = DateTimeFormatter.ofPattern("yy.MM.dd.HHmm", Locale.CHINA)
+    // 格式化后的日期输出为版本号
+    return currentDate.format(versionDateTimeFormatter)
+}
+
+// 生成始终增加的VersionCode
+fun calculateVersionCode(): Int {
+    // 使用时间戳（秒）
+    val timestampCode = (System.currentTimeMillis() / 1000).toInt()
+    println("最新VersionCode: $timestampCode")
+    return timestampCode
+}
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.customtv.diliblay"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.customtv.diliblay"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = calculateVersionCode()
+        versionName = getVersionName()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    // 自定义 APK 输出文件名
+    androidComponents {
+        onVariants { variant ->
+            variant.outputs.forEach { output ->
+                // 构建新的文件名，大写后缀名以防止QQ添加.1
+                if (output is com.android.build.api.variant.impl.VariantOutputImpl) {
+                    output.outputFileName.set("DiliBlay删档内测${output.versionName.get()}.APK")
+                    println("已修改文件名为${output.outputFileName.get()}")
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
